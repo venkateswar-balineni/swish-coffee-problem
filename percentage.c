52778e73c759b32ffc0ccbfb6fbf9b26a0b9ef97
venkateswar-balineni <53931295+venkateswar-balineni@users.noreply.github.com>
2019-08-09 12:23:41 +0530

    Add files via upload

---
+++ percentage.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main()
+{
+float amount,percent,value,sum;
+
+scanf("%f %f",&amount,&percent);
+
+sum=0;
+
+while(amount!=0)
+
+	{
+
+    		sum=sum+amount;
+		
+value=(percent/100)*amount;
+		
+amount=amount-value;
+	
+}
+
+printf("%.f",sum);
+
+    return 0;
+
+}
